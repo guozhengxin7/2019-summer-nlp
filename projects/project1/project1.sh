d64ccb2b1c4b2013e0f72b116c2e9afa44148966
Ronaldinho <ronalinho_x86@qq.com>
2019-10-21 00:11:08 +0800

    update,

---
+++ projects/project1/project1.sh
@@ -0,0 +1,26 @@
+
+
+
+把文中每个人的核心观点整理出来，总结成表格
+    人物
+    观点
+    来源
+
+
+1 搜索 说 的近义词
+
+2 词性标注
+
+3 依存分析
+
+
+
+
+
+数据库为云数据库，配置为MySQL；请大家下载DataGrip或者MySQL WorkBench进行数据库的访问，如果不会下载或者使用的请大家参考项目一的指导文件（Trello上有）；
+访问地址和密码为：
+数据库地址（Host）：rm-8vbwj6507z6465505ro.mysql.zhangbei.rds.aliyuncs.com
+用户名（User）：root
+用户密码（Password）：AI@2019@ai
+数据库名（Database）：stu_db
+表名：news_chinese
